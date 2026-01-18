49d91ee582dfba0ccf61d6d7990d8206c80ec296
nandinisri1912 <nandinisri0929@gmail.com>
2026-01-18 18:25:30 +0530

    Add Team Page

---
+++ team.js
@@ -0,0 +1,9 @@
+function toggleCard(card) {
+    const content = card.querySelector(".expandable");
+
+    document.querySelectorAll(".expandable").forEach(c => {
+        if (c !== content) c.classList.remove("active");
+    });
+
+    content.classList.toggle("active");
+}
